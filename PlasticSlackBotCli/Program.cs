08030c7ffe74f9e84ab7f5590da26d94c8f36b00
pablo <pablo@.(none)>
2015-01-28 13:30:29 +0100

    added support for a CLI based "bot"

---
+++ PlasticSlackBotCli/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Slack.API.Services;
+using SlackBot.Portable.Services;
+
+namespace PlasticSlackBotCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Usage();
+                return;
+            }
+
+            string token = args[0];
+
+            PlasticCMDService plasticCmd = new PlasticCMDService();
+
+            SlackRTMService service = new SlackRTMService(plasticCmd);
+
+            service.SlackDataReceived += slackService_SlackDataReceived;
+
+            service.ConnectPlasticSlackBot(token);
+
+            Console.WriteLine("Type ENTER to exit");
+
+            Console.ReadLine();
+        }
+
+        static void slackService_SlackDataReceived(object sender, SlackBot.Portable.Model.SlackEventArgs e)
+        {
+            Console.WriteLine(e.Data.Text);
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("PlasticSlackBotCli is a Slack bot to run Plastic commands hosted on a CLI app");
+            Console.WriteLine("Usage: PlasticSlackBotCli slacktoken");
+        }
+    }
+}
